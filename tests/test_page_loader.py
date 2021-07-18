477da046b19006d2ec34beefb7dfff0c211f23e8
elena <lenok.ok.lol@gmail.com>
2021-07-19 00:44:31 +0500

    deleted wrong test

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -36,36 +36,3 @@ def test_page_downloader():
             dc = filecmp.dircmp(testdir_path,
                                 downloaded_path.parent / 'ru-hexlet-io-courses_files')
             assert len(dc.left_only + dc.right_only) == 0
-
-
-def test_page_2_downloader():
-    page_link = 'https://site.com/blog/about'
-    link_link = '/blog/about/assets/styles.css'
-    img_link = '/photos/me.jpg'
-    script_link = 'https://site.com/assets/scripts.js'
-
-    with open('tests/fixtures/test_page_2_content/site-com-blog-about_files/site-com-blog-about.html', 'r') as f:
-        page = f.read()
-    with open('tests/fixtures/test_page_2_content/site-com-blog-about_files/site-com-blog-about-assets-styles.css', 'rb') as f:
-        link = f.read()
-    with open('tests/fixtures/test_page_2_content/site-com-blog-about_files/site-com-photos-me.jpg', 'rb') as f:
-        img = f.read()
-    with open('tests/fixtures/test_page_2_content/site-com-blog-about_files/site-com-assets-scripts.js', 'rb') as f:
-        script = f.read()
-    testfile_path = Path.cwd() / 'tests' / 'fixtures' / 'test_page_2_content' / 'site-com-blog-about.html'
-    testdir_path = Path.cwd() / 'tests' / 'fixtures' / 'test_page_2_content' / 'site-com-blog-about_files'
-
-    with requests_mock.Mocker() as m:
-        m.get(page_link, text=page)
-        m.get(link_link, content=link)
-        m.get(img_link, content=img)
-        m.get(script_link, content=script)
-        download(page_link, '/Users/Elena/dev/examples')
-        with tempfile.TemporaryDirectory() as tmpdir:
-
-            downloaded_path = PosixPath(download(page_link, tmpdir))
-            assert filecmp.cmp(testfile_path, downloaded_path, shallow=False)
-            dc = filecmp.dircmp(testdir_path,
-                                downloaded_path.parent / 'site-com-blog-about_files')
-            assert len(dc.left_only + dc.right_only) == 0
-
